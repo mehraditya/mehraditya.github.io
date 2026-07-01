e04a113fe4505d889930754e0adb067abf0d0c5b
Aditya Mehra <adityamehra@Adityas-MacBook-Air.local>
2026-07-01 11:57:52 +0530

    feat: add Navbar

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return(
+     <nav className='nav'>
+        <div className='brand'>Research Notebook</div>
+        <div className='links'><a>Research</a><a>Reading</a><a>Projects</a><a>About</a><a href='https://github.com/mehraditya'>GitHub</a></div>
+    </nav>
+    )
+}

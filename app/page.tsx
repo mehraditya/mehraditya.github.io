200904c5d5d70d9111437ca79991d0ec858dd5d5
Aditya Mehra <adityamehra@Adityas-MacBook-Air.local>
2026-07-01 11:25:05 +0530

    refactor: add new utils and page information

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import Navbar from '@/components/layout/Navbar';
+import Container from '@/components/layout/Container';
+import Hero from '@/components/home/Hero';
+import CurrentReading from '@/components/home/CurrentReading';
+import RecentNotes from '@/components/home/RecentNotes';
+import Footer from '@/components/layout/Footer';
+
+export default function Home(){
   return (
-    <main>
-      <h1>Research Notebook</h1>
-    </main>
+    <>
+      <Navbar/>
+      <Container>
+        <Hero/>
+        <CurrentReading/>
+        <RecentNotes/>
+      </Container>
+      <Footer/>
+    </>
   );
 }
